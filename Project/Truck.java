af5d16cea1ccadcf866d4b85f1c2b1b55e43e512
Karol Olbryś  <karlson2002@gmail.com>
2024-06-02 18:11:59 +0200

    Created classes and implemented new methods

---
+++ Project/Truck.java
@@ -0,0 +1,22 @@
+package Project;
+
+public class Truck extends Vehicle{
+
+    protected double milesDriven;
+    protected double fuelConsumed;
+    protected double cargoWeight;
+
+    public Truck(String make, String model, int year, double milesDriven, double fuelConsumed, double cargoWeight) {
+        super(make, model, year);
+        this.milesDriven = milesDriven;
+        this.fuelConsumed = fuelConsumed;
+        this. cargoWeight = cargoWeight;
+    }
+
+    @Override
+    public double calculateFuelEfficiency() {
+        double fuelEfficiency = milesDriven/(fuelConsumed + (cargoWeight * 0.5));
+        return fuelEfficiency;
+    }
+    
+}
---
+++ Project/Vehicle.java
@@ -0,0 +1,15 @@
+package Project;
+
+public abstract class Vehicle {
+    protected String make;
+    protected String model;
+    protected int year;
+
+    public Vehicle(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    public abstract double calculateFuelEfficiency();
+}
